4a029febcf9b9c3ae9d9b4eab17e7572533dc223
16046512 <16046512@myrp.edu.sg>
2018-05-13 11:46:22 +0800

    regular expression

--- app/src/main/java/com/example/a16022596/lifespeechkidzo/RegisterPage.java
+++ app/src/main/java/com/example/a16022596/lifespeechkidzo/RegisterPage.java
@@ -8,6 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -20,12 +21,13 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class RegisterPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class RegisterPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     //sidebar start
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
-    private EditText etLastname,etFirstname,etPassword,etEmail;
+    private EditText etLastname, etFirstname, etPassword, etEmail, etUsername, etPassword2;
     private Button btnSubmit;
+
     //sidebar end
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,16 +47,13 @@ public class RegisterPage extends AppCompatActivity implements NavigationView.On
 
         //sidebar end
 
-        etFirstname = (EditText)findViewById(R.id.regEtFirstName);
-        etLastname = (EditText)findViewById(R.id.regEtLastName);
-        etEmail = (EditText)findViewById(R.id.regEtEmail);
-        etPassword = (EditText)findViewById(R.id.regEtPwd);
+        etFirstname = (EditText) findViewById(R.id.regEtFirstName);
+        etLastname = (EditText) findViewById(R.id.regEtLastName);
+        etUsername = (EditText) findViewById(R.id.regEtUserName);
+        etEmail = (EditText) findViewById(R.id.regEtEmail);
+        etPassword = (EditText) findViewById(R.id.regEtPwd1);
+        etPassword2 = (EditText) findViewById(R.id.regEtPwd2);
         btnSubmit = (Button) findViewById(R.id.btnRegister);
-
-
-
-
-
 
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -62,92 +61,142 @@ public class RegisterPage extends AppCompatActivity implements NavigationView.On
             @Override
             public void onClick(View arg0) {
 
-                if (!etFirstname.getText().toString().equals("") &&!etLastname.getText().toString().equals("") && !etEmail.getText().toString().equals("") && !etPassword.getText().toString().equals("")) {
+                check();
+            }
+        });
+    }
 
+
+    private void check() {
+
+        final String firstname = etFirstname.getText().toString().trim();
+        final String lastname = etLastname.getText().toString().trim();
+        final String username = etUsername.getText().toString().trim();
+        final String email = etEmail.getText().toString().trim();
+        final String firstpassword = etPassword.getText().toString().trim();
+        final String secondpassword = etPassword2.getText().toString().trim();
+
+
+        if (firstname.isEmpty()) {
+            etFirstname.setError("First name is required");
+            etFirstname.requestFocus();
+            return;
+        }
+        if (lastname.isEmpty()) {
+            etLastname.setError("Last name is required");
+            etLastname.requestFocus();
+            return;
+        }
+        if (username.isEmpty()) {
+            etUsername.setError("Username is required");
+            etUsername.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("Please enter a valid email");
+            etEmail.requestFocus();
+            return;
+        }
+
+
+        if (!firstpassword.matches("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[~()#;'?!@$%.^&*-]).{8,}$")) {
+            etPassword.setError("Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character.");
+            etPassword.requestFocus();
+            return;
+        }
+        if (firstpassword.isEmpty()) {
+            etPassword.setError("Password is required");
+            etPassword.requestFocus();
+            return;
+        }
+
+        if (secondpassword.length() < 8) {
+            etPassword2.setError("Minimum length of password should be 8");
+            etPassword2.requestFocus();
+            return;
+        }
+        if (secondpassword.isEmpty()) {
+            etPassword2.setError("Password is required");
+            etPassword2.requestFocus();
+            return;
+        }
+        if (!firstpassword.equals(secondpassword)) {
+            etPassword.setError("Password does not match");
+            etPassword.requestFocus();
+            etPassword2.setError("Password does not match");
+            etPassword2.requestFocus();
+            return;
+        } else {
+
+
+            Toast.makeText(getApplicationContext(),
+                    "Please wait",
+                    Toast.LENGTH_SHORT).show();
+
+            String url = "https://lifespeechsample3.000webhostapp.com/doRegister.php?firstName=" + etFirstname.getText().toString() + "&lastName=" + etLastname.getText().toString() + "&email=" + etEmail.getText().toString() + "&username=" + etLastname.getText().toString() + "&password=" + etPassword.getText().toString() + "&username="+etUsername.getText().toString()+"";
+            AsyncHttpClient client = new AsyncHttpClient();
+            client.get(url, new AsyncHttpResponseHandler() {
+
+                @Override
+                public void onFailure(Throwable arg0, String arg1) {
                     Toast.makeText(getApplicationContext(),
-                            "Please wait",
-                            Toast.LENGTH_SHORT).show();
-
-                    String url = "https://lifespeechsample3.000webhostapp.com/doRegister.php?firstName="+etFirstname.getText().toString()+"&lastName="+etLastname.getText().toString()+"&email="+etEmail.getText().toString()+"&username="+etLastname.getText().toString()+"&password="+etPassword.getText().toString()+"";
-                    AsyncHttpClient client = new AsyncHttpClient();
-                    client.get(url, new AsyncHttpResponseHandler() {
-
-                        @Override
-                        public void onFailure(Throwable arg0, String arg1) {
-                            Toast.makeText(getApplicationContext(),
-                                    "Please connect to internet and try again",
-                                    Toast.LENGTH_LONG).show();
-
-
-                        }
-
-                        @Override
-                        public void onFinish() {
-
-                            super.onFinish();
-                        }
-
-                        @Override
-                        public void onStart() {
-
-                            super.onStart();
-                        }
-
-                        @Override
-                        public void onSuccess(String response) {
-                            try {
-                                JSONObject jsonObj = new JSONObject(response);
-                                JSONObject resultObject = jsonObj.getJSONObject("result");
-                                String username = resultObject.getString("usernamesuccess");
-                                Toast.makeText(getApplicationContext(),
-                                        username,
-                                        Toast.LENGTH_LONG).show();
-                                if (username.equals("1")){
-                                    Toast.makeText(getApplicationContext(),
-                                            "Sorry user name has been taken",
-                                            Toast.LENGTH_LONG).show();
-
-                                }else{
-                                    Toast.makeText(getApplicationContext(),
-                                            "Account create successful please verify your email",
-                                            Toast.LENGTH_LONG).show();
-
-                                }
-
-                            } catch (JSONException e) {
-
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-                } else {
-                    Toast.makeText(getApplicationContext(),
-                            "Please complete form!",
+                            "Please connect to internet and try again",
                             Toast.LENGTH_LONG).show();
+
 
                 }
 
-            }
-        });
+                @Override
+                public void onFinish() {
+
+                    super.onFinish();
+                }
+
+                @Override
+                public void onStart() {
+
+                    super.onStart();
+                }
+
+                @Override
+                public void onSuccess(String response) {
+                    try {
+                        JSONObject jsonObj = new JSONObject(response);
+                        JSONObject resultObject = jsonObj.getJSONObject("result");
+                        String username = resultObject.getString("usernamesuccess");
+                        Toast.makeText(getApplicationContext(),
+                                username,
+                                Toast.LENGTH_LONG).show();
+                        if (username.equals("1")) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Sorry user name has been taken",
+                                    Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "Account create successful please verify your email",
+                                    Toast.LENGTH_LONG).show();
+
+                        }
+
+                    } catch (JSONException e) {
+
+                        e.printStackTrace();
+                    }
+                }
+            });
 
 
-
-
-
-
-
-
-
-
-
-
-
+        }
     }
+
+
     //sidebar start
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)){return true;
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -155,28 +204,25 @@ public class RegisterPage extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.nav_home){
+        if (id == R.id.nav_home) {
             Intent mainActivityIntent = new Intent(RegisterPage.this, MainActivity.class);
             startActivity(mainActivityIntent);
 
-        }else if(id == R.id.nav_Login) {
+        } else if (id == R.id.nav_Login) {
 
             Intent mainActivityIntent = new Intent(RegisterPage.this, LoginPage.class);
             startActivity(mainActivityIntent);
-        }
-
-        else if (id == R.id.nav_CreateAccount){
+        } else if (id == R.id.nav_CreateAccount) {
             Intent mainActivityIntent = new Intent(RegisterPage.this, RegisterPage.class);
             startActivity(mainActivityIntent);
 
 
-        }
-        else if (id == R.id.nav_contact){
+        } else if (id == R.id.nav_contact) {
 
             Intent mainActivityIntent = new Intent(RegisterPage.this, ContactPage.class);
             startActivity(mainActivityIntent);
 
-        }else if(id == R.id.nav_about){
+        } else if (id == R.id.nav_about) {
 
             Intent rpIntent = new Intent(Intent.ACTION_VIEW);
             // Set the URL to be used.
