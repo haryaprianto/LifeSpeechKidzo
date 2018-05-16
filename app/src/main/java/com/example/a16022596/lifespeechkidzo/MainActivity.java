7c94774e5c56d9b6657b1b2828e07a4434efc941
16046512 <16046512@myrp.edu.sg>
2018-05-16 12:27:16 +0800

    regular expression

--- app/src/main/java/com/example/a16022596/lifespeechkidzo/MainActivity.java
+++ app/src/main/java/com/example/a16022596/lifespeechkidzo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a16022596.lifespeechkidzo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -8,28 +11,25 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
+import android.util.TypedValue;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     //sidebar start
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     //sidebar end
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
 
         //sidebar start
@@ -42,26 +42,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setItemTextColor(null);
         navigationView.setItemTextAppearance(R.style.MenuTextStyle);
-
         //sidebar end
-
-
 
 
     }
 
 
-
-
-
-
-
-
-
     //sidebar start
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)){return true;
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -69,30 +60,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.nav_home){
+        if (id == R.id.nav_home) {
             Intent mainActivityIntent = new Intent(MainActivity.this, MainActivity.class);
             startActivity(mainActivityIntent);
 
 
-        }else if(id == R.id.nav_Login) {
+        } else if (id == R.id.nav_Login) {
 
             Intent mainActivityIntent = new Intent(MainActivity.this, LoginPage.class);
             startActivity(mainActivityIntent);
-        }
-
-        else if (id == R.id.nav_CreateAccount){
+        } else if (id == R.id.nav_CreateAccount) {
             Intent mainActivityIntent = new Intent(MainActivity.this, RegisterPage.class);
             startActivity(mainActivityIntent);
 
 
-        }
-        else if (id == R.id.nav_contact){
+        } else if (id == R.id.nav_contact) {
 
             Intent mainActivityIntent = new Intent(MainActivity.this, ContactPage.class);
             startActivity(mainActivityIntent);
 
 
-        }else if(id == R.id.nav_about){
+        } else if (id == R.id.nav_about) {
 
             Intent rpIntent = new Intent(Intent.ACTION_VIEW);
             // Set the URL to be used.
